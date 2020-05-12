8e1b5e20278ece7f58faeef237efe2aa957103b1
Richard Huynh <huynhtastic@gmail.com>
2020-05-12 17:51:41 -0500

    Show user brew preferences in list

--- lib/screens/app/app.dart
+++ lib/screens/app/app.dart
@@ -1,26 +1,45 @@
 import 'package:flutter/material.dart';
+import 'package:the_dojo/models/brew.dart';
 import 'package:the_dojo/services/authentication.dart';
+import 'package:the_dojo/services/database.dart';
+import 'package:provider/provider.dart';
+
+import 'brew_list.dart';
 
 class App extends StatelessWidget {
   final _auth = AuthenticationService();
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      backgroundColor: Colors.brown[50],
-      appBar: AppBar(
-        title: Text('The Dojo'),
-        elevation: 0.0,
-        backgroundColor: Colors.brown[400],
-        actions: <Widget>[
-          FlatButton.icon(
-            icon: Icon(Icons.person),
-            label: Text('Logout'),
-            onPressed: () async {
-              await _auth.signOut();
-            },
+    return StreamProvider<List<Brew>>.value(
+      value: DatabaseService().brews,
+      catchError: (_, err) {
+        print(err.toString());
+        return [
+          Brew(
+            name: '',
+            strength: 100,
+            sugars: '',
           )
-        ],
+        ];
+      },
+      child: Scaffold(
+        backgroundColor: Colors.brown[50],
+        appBar: AppBar(
+          title: Text('The Dojo'),
+          elevation: 0.0,
+          backgroundColor: Colors.brown[400],
+          actions: <Widget>[
+            FlatButton.icon(
+              icon: Icon(Icons.person),
+              label: Text('Logout'),
+              onPressed: () async {
+                await _auth.signOut();
+              },
+            )
+          ],
+        ),
+        body: BrewList(),
       ),
     );
   }
--- lib/services/database.dart
+++ lib/services/database.dart
@@ -1,4 +1,5 @@
 import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:the_dojo/models/brew.dart';
 
 class DatabaseService {
   final String uid;
@@ -14,5 +15,21 @@ class DatabaseService {
       'name': name,
       'strength': strength,
     });
+  }
+
+  // brew list from snapshot
+  List<Brew> _brewListFromSnapshot(QuerySnapshot snapshot) {
+    return snapshot.documents.map((doc) {
+      return Brew(
+        name: doc.data['name'] ?? '',
+        strength: doc.data['strength'] ?? 0,
+        sugars: doc.data['sugars'] ?? '',
+      );
+    }).toList();
+  }
+
+  // get brews stream
+  Stream<List<Brew>> get brews {
+    return brewCollection.snapshots().map(_brewListFromSnapshot);
   }
 }
